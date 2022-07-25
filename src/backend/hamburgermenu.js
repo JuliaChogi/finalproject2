a98d8b49505341d5e292319ef107b8a062e74ce3
JuliaChogi <jwolf2019@yandex.ru>
2022-07-25 15:59:20 +0300

    almost final
    111

---
+++ src/backend/hamburgermenu.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector('.burger')
+const navMenu = document.querySelector('.nav_list')
+const body = document.querySelector('body')
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active')
+    navMenu.classList.toggle('active')
+    body.classList.toggle('active')
+})
+
+document.querySelectorAll('.link, .nav_sign-up, .nav_sign-in').forEach(n => n.addEventListener('click', () => {
+    burger.classList.remove('active')
+    navMenu.classList.remove('active')
+    body.classList.remove('active')
+}))
